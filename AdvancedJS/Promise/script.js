d94824a2cdf1f8f1b214e0c7822c180434beeac0
MrKalyanKing <badhavathkalyan08@gmail.com>
2026-03-07 16:48:16 +0530

    Refactor promise example to clarify asynchronous behavior and add implicit function demonstration

--- AdvancedJS/Promise/script.js
+++ AdvancedJS/Promise/script.js
@@ -1,26 +1,39 @@
 
-// let pom=new Promise((resolve,rej)=>{
-//     return rej(()=>{
-//         fetch('https://jsonplaceholder.typicode.com/todos/1')
-//       .then(response => response.json())
-//       .then(json => console.log(json))
-//     })
-// })
-// console.log("hello");
-let pom = new Promise((resolve, reject) => {
-    fetch('https://jsonplaceholder.typicode.com/todos/1')
-});
 
-console.log("hello");
+//promise are main JS class 
+// promise handler went to the micro task queue (.then() and .catch()) these perform the asynchronuous tasks
+
+let pom=new Promise((resolve,rej)=>{
+     
+    let party=false
+    if(party){
+        return resolve({destination:"Taj hotel"})
+    }
+    return rej("No party")
+})
+
+
+
+
+// let pom = new Promise((resolve, reject) => {
+//     fetch('https://jsonplaceholder.typicode.com/todos/1')
+// });
+
+// console.log("hello");
 
 // pom
 // .then(data => console.log(data))
 // .catch(err => console.log(err));
 
-console.log("hello");
-console.log(pom);
-console.log("hello");
-// promise is not actaullly Aysnc but it is class of JS
+
+
+// console.log("hello");
+// console.log(pom);
+// console.log("hello");
+
+
+
+// promise is not actaullly Aysnc but it is class of JS ***
 //
 
 
@@ -28,3 +41,24 @@ console.log("hello");
 // so that 
 // If we create a Promise and return resolve(fetch()), and inside it we use an API to fetch data, will that operation be placed in the macrotask queue?
 // so here promise will get exexuted after all the logs 
+
+
+
+//** */ promise handlers are two types ***
+// before es6 
+//  1 st type
+// .then() and .catch()
+
+//implcit function
+pom.then((val)=>console.log(val))
+.catch((err)=>console.log(err))
+
+console.log("hello");
+
+setTimeout(()=>{
+    console.log("yeepp");
+    
+},0)
+console.log("Hello after timeout");
+
+
