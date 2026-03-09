2ffb12d6e960e8191e4d8b81a01f3c145fbb7401
MrKalyanKing <badhavathkalyan08@gmail.com>
2026-03-09 21:06:41 +0530

    Add fetch example to demonstrate handling API responses

---
+++ AdvancedJS/FetchApi/script.js
@@ -0,0 +1,15 @@
+
+
+
+let response=fetch("https://fakestoreapi.com/products")
+
+response.then((val)=> val.json() )
+.then((val)=>console.log(val))
+
+
+//here fetch will accept the data in the chunks it is low level design based on the browser (default browser)
+// thus why all the chunks will get together converts into readable stream
+// default behaviour of browser will accept the data in the form of chunks
+
+//streams 
+//here readable is browser part it return the promise
